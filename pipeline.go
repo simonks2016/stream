680033a4565e07ab7bfc2a9008c7a7fa8f8854fe
simonks@163.com <simonks@163.com>
2026-04-19 18:04:34 +0800

    version 0.1.0 beta

--- pipeline.go
+++ pipeline.go
@@ -7,6 +7,44 @@ import (
 	"github.com/simonks2016/stream/stream"
 )
 
-func NewPipeline(ctx context.Context) stream.Pipeline {
-	return runtime.NewRuntime(ctx, 100)
+func NewPipeline(ctx context.Context, opts ...PipelineOption) stream.Pipeline {
+
+	allowed_lateness := int64(100)
+	if len(opts) > 0 {
+
+		for _, opt := range opts {
+			if opt.Name() == "allowed_lateness" {
+				al, ok := opt.Value().(int64)
+				if ok {
+					allowed_lateness = al
+				}
+			}
+		}
+	}
+	return runtime.NewRuntime(ctx, allowed_lateness)
+}
+
+type PipelineOption interface {
+	Name() string
+	Value() interface{}
+}
+
+type customPipelineOption struct {
+	name  string
+	value interface{}
+}
+
+func (o customPipelineOption) Name() string {
+	return o.name
+}
+func (o customPipelineOption) Value() interface{} {
+	return o.value
+}
+
+func NewPipelineOption(name string, value interface{}) PipelineOption {
+	return customPipelineOption{name, value}
+}
+
+func WithAllowedLateness(ms int64) PipelineOption {
+	return NewPipelineOption("allowed_lateness", ms)
 }
