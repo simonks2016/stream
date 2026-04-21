8d11e8708e29a7f4532dc60b7f71a511288fa669
simonks@163.com <simonks@163.com>
2026-04-21 13:41:03 +0800

    version 0.1.0 beta

--- stream/processor.go
+++ stream/processor.go
@@ -4,6 +4,15 @@ import (
 	"context"
 )
 
+type MultiOutputProcessor[I any, O any] interface {
+	Process(ctx context.Context, msg Message[I]) ([]Output[O], error)
+}
+
+type Output[T any] struct {
+	Endpoint Endpoint
+	Message  Message[T]
+}
+
 type Processor[in any, out any] interface {
 	Process(ctx context.Context, in Message[in]) (Endpoint, Message[out], bool, error)
 }
--- wrapper.go
+++ wrapper.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 
 	"context"
+
 	"github.com/simonks2016/stream/stream"
 )
 
@@ -47,6 +48,70 @@ func WrapProcessor[I any, O any](processor stream.Processor[I, O]) stream.Handle
 				Ts:          out.Ts,
 				Payload:     out.Payload,
 			})
+		}
+		return nil
+	}
+}
+
+func WrapMultiOutputProcessor[I any, O any](
+	processor stream.MultiOutputProcessor[I, O],
+) stream.Handler {
+	return func(ctx context.Context, msg stream.Message[any], sink stream.Sink) error {
+		if msg.IsEmpty() {
+			return nil
+		}
+
+		payload, ok := msg.Payload.(I)
+		if !ok {
+			var zeroI I
+
+			gotType := "<nil>"
+			if msg.Payload != nil {
+				gotType = reflect.TypeOf(msg.Payload).String()
+			}
+
+			wantType := "<unknown>"
+			if t := reflect.TypeOf(zeroI); t != nil {
+				wantType = t.String()
+			}
+
+			return fmt.Errorf(
+				"payload type mismatch, key=%s, got=%s, want=%s",
+				msg.Key,
+				gotType,
+				wantType,
+			)
+		}
+
+		ingress := stream.Message[I]{
+			Payload:     payload,
+			Ts:          msg.Ts,
+			Key:         msg.Key,
+			WatermarkTs: msg.WatermarkTs,
+			IngestTime:  msg.IngestTime,
+			SinkTime:    msg.SinkTime,
+		}
+
+		outs, err := processor.Process(ctx, ingress)
+		if err != nil {
+			return err
+		}
+
+		for _, out := range outs {
+			if out.Endpoint.Kind == stream.NullEndpointKind {
+				continue
+			}
+			// 发送信息
+			if err = sink(out.Endpoint, stream.Message[any]{
+				Payload:     out.Message.Payload,
+				Ts:          out.Message.Ts,
+				Key:         out.Message.Key,
+				WatermarkTs: out.Message.WatermarkTs,
+				IngestTime:  out.Message.IngestTime,
+				SinkTime:    out.Message.SinkTime,
+			}); err != nil {
+				return err
+			}
 		}
 		return nil
 	}
